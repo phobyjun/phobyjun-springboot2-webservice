97ac9a44c5638d66e3e860e6e2a4ea1c38f9c056
phobyjun <phobyjun@gmail.com>
2022-02-10 02:17:15 +0900

    ADD: contents to Chapter3.

---
+++ src/main/java/com/phobyjun/springboot/service/posts/PostsService.java
@@ -0,0 +1,38 @@
+package com.phobyjun.springboot.service.posts;
+
+import com.phobyjun.springboot.domain.posts.Posts;
+import com.phobyjun.springboot.domain.posts.PostsRepository;
+import com.phobyjun.springboot.web.dto.PostsResponseDto;
+import com.phobyjun.springboot.web.dto.PostsSaveRequestDto;
+import com.phobyjun.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ src/main/java/com/phobyjun/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.phobyjun.springboot.web;
+
+import com.phobyjun.springboot.service.posts.PostsService;
+import com.phobyjun.springboot.web.dto.PostsResponseDto;
+import com.phobyjun.springboot.web.dto.PostsSaveRequestDto;
+import com.phobyjun.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
