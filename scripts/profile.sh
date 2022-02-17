b94dcb037d0ccaa13cc00b90a365d800cca68839
phobyjun <phobyjun@gmail.com>
2022-02-17 19:27:56 +0900

    FIX: typo

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -23,9 +23,9 @@ function find_idle_profile()
 
 function find_idle_port()
 {
-  IDLE_PROFILE=$find_idle_profile)
+  IDLE_PROFILE=$(find_idle_profile)
 
-  if [ ${IDLE_PROFILE} == real 1]
+  if [ ${IDLE_PROFILE} == real1 ]
   then
     echo "8081"
   else
